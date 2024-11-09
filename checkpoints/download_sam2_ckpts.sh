eda2c7e29b02d16f89000a2ab6f140ef54897b87
PIKA <793806542@qq.com>
2024-11-09 14:29:58 +0800

    Update download_sam2_ckpts.sh

--- checkpoints/download_sam2_ckpts.sh
+++ checkpoints/download_sam2_ckpts.sh
@@ -16,14 +16,14 @@ sam2_hiera_l_url="${BASE_URL}sam2_hiera_large.pt"
 
 
 # Download each of the four checkpoints using wget
-echo "Downloading sam2_hiera_tiny.pt checkpoint..."
-wget $sam2_hiera_t_url || { echo "Failed to download checkpoint from $sam2_hiera_t_url"; exit 1; }
+#echo "Downloading sam2_hiera_tiny.pt checkpoint..."
+#wget $sam2_hiera_t_url || { echo "Failed to download checkpoint from $sam2_hiera_t_url"; exit 1; }
 
-echo "Downloading sam2_hiera_small.pt checkpoint..."
-wget $sam2_hiera_s_url || { echo "Failed to download checkpoint from $sam2_hiera_s_url"; exit 1; }
+#echo "Downloading sam2_hiera_small.pt checkpoint..."
+#wget $sam2_hiera_s_url || { echo "Failed to download checkpoint from $sam2_hiera_s_url"; exit 1; }
 
-echo "Downloading sam2_hiera_base_plus.pt checkpoint..."
-wget $sam2_hiera_b_plus_url || { echo "Failed to download checkpoint from $sam2_hiera_b_plus_url"; exit 1; }
+#echo "Downloading sam2_hiera_base_plus.pt checkpoint..."
+#wget $sam2_hiera_b_plus_url || { echo "Failed to download checkpoint from $sam2_hiera_b_plus_url"; exit 1; }
 
 echo "Downloading sam2_hiera_large.pt checkpoint..."
 wget $sam2_hiera_l_url || { echo "Failed to download checkpoint from $sam2_hiera_l_url"; exit 1; }
